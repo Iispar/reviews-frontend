75479f05fdc33560da9af66f8f07eb8590858c35
Iiro Partanen <iispar@utu.fi>
2023-05-13 08:08:39 +0300

    removed create-react-app layout and added eslint

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+/**
+   * Basic structure for the application. Routes for the normal home view with all stations and
+   * journeys and a route for the single station view with data for station and map.
+   */
+function App() {
+  return <div> Hello World </div>;
+}
+
+export default App;
