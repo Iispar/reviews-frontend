176a4306043428a4671fc28a0e447600ef7792ab
Iiro Partanen <iispar@utu.fi>
2023-05-21 15:40:37 +0300

    listeners to input fields

--- src/pages/login/CreateAccountForm.jsx
+++ src/pages/login/CreateAccountForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, React } from 'react';
 import $ from 'jquery';
 import InputField from '../../components/InputField';
 import useCreateAccount from './useCreateAccount';
@@ -8,6 +8,8 @@ import useCreateAccount from './useCreateAccount';
  * @returns create account form
  */
 const CreateAccountForm = () => {
+  const [password, setPassword] = useState('null');
+  const [confirmPassword, setConfirmPassword] = useState('null');
   /**
    * changes view between create account and login view.
    */
@@ -17,6 +19,19 @@ const CreateAccountForm = () => {
     $('#createNew__createAccountForm__inputs__form').trigger('reset');
   };
 
+  /**
+   * Informs the user of a succesful login. Also resets the form and moves to login page.
+   */
+  const succesfulCreation = () => {
+    $('#createNew__createAccountForm__inputs__form').trigger('reset');
+    console.log('succesful creation');
+    changeView();
+  };
+
+  /**
+   * creates a new account.
+   * @param username, password, email, name
+   */
   const createAccount = (e) => {
     e.preventDefault();
     const values = e.target;
@@ -26,8 +41,44 @@ const CreateAccountForm = () => {
       values[2].value,
       values[3].value,
       values[4].value,
-    ));
+    )) succesfulCreation();
   };
+
+  /**
+   * Loads listeners to input fields when the app loads fully.
+   */
+  $(document).ready(() => {
+    /**
+     * Checks the email is the correct format
+     */
+    $('#createEmail').on('change', (val) => {
+      const string = val.target.value;
+      const regex = /^\w*@\w*\..\w*/;
+      if (regex.test(string)) console.log('true');
+      else console.log('needs to be format email@something.com');
+    });
+
+    /**
+     * Checks the passwords match
+     */
+    $('#createPassword').on('change', (val) => {
+      const input = val.target.value;
+      setPassword(input);
+      if (input !== confirmPassword) console.log('passwords don\'t match');
+      else console.log('passwords match');
+    });
+
+    /**
+     * Checks the passwords match.
+     */
+    $('#createConfirmPassword').on('change', (val) => {
+      const input = val.target.value;
+      setConfirmPassword(input);
+      if (input !== password) console.log('passwords don\'t match');
+      else console.log('passwords match');
+    });
+  });
+
   return (
     <div className="createNew" id="createNew">
       <div className="createNew__createAccountForm">
@@ -35,7 +86,7 @@ const CreateAccountForm = () => {
         <div className="createNew__createAccountForm__inputs">
           <form className="createNew__createAccountForm__inputs__form" id="createNew__createAccountForm__inputs__form" onSubmit={(e) => createAccount(e)}>
             <InputField id="createUsername" title="username" width="280px" />
-            <InputField id="createName" title="name" width="280px" />
+            <InputField id="createName" title="first name" width="280px" />
             <InputField id="createEmail" title="email" width="280px" />
             <InputField id="createPassword" type="password" title="password" width="280px" />
             <InputField id="createConfirmPassword" type="password" title="confirm password" width="280px" />
