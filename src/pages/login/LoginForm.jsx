0a6c029538d67a2aadaf287de88e055e0d0f8f2e
Iiro Partanen <iispar@utu.fi>
2023-05-21 13:05:16 +0300

    mock useLogin hook for login

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 import $ from 'jquery';
 import { useState, React } from 'react';
 import { useNavigate } from 'react-router-dom';
 import InputField from '../../components/InputField';
+import useLogin from './useLogin';
 
 /**
  * creates the sign in form.
@@ -42,8 +44,10 @@ const LoginForm = () => {
   /**
    * Handles the login and moves to home page.
    */
-  const login = () => {
-    navigate('/home');
+  const login = (e) => {
+    e.preventDefault();
+    const values = e.target.form;
+    if (useLogin(values[0].value, values[1].value)) navigate('/home');
   };
 
   return (
@@ -64,7 +68,7 @@ const LoginForm = () => {
               </form>
             </div>
           </div>
-          <button type="submit" className="login__loginForm__inputs__loginBtn" form="loginForm__inputs__form" onClick={() => login()}> login </button>
+          <button type="submit" className="login__loginForm__inputs__loginBtn" form="loginForm__inputs__form" onClick={(e) => login(e)}> login </button>
         </div>
       </div>
       <div className="login__createAccount">
---
+++ src/pages/login/useLogin.js
@@ -0,0 +1,7 @@
+const useFetch = (username, password) => {
+  console.log(`Login with username: ${username}, password: ${password}`);
+
+  return true;
+};
+
+export default useFetch;
