d319d1b12c79aeaa217f207f023283792fed350c
Iiro Partanen <iispar@utu.fi>
2023-05-22 18:22:49 +0300

    basic nav bar withour functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import { useState, React } from 'react';
+import $ from 'jquery';
 
-// eslint-disable-next-line arrow-body-style
 const Header = () => {
-  return <div> header </div>;
+  const [navState, setNavState] = useState(false);
+  /**
+   * Toggles the nav bar on and off when the hamburger menu is clicked.
+   */
+  const toggleNavBar = () => {
+    if (navState) {
+      $('#navBar').css('transform', 'scaleX(0)');
+      setNavState(false);
+      $('#header__hamburger').removeClass('clicked');
+    } else {
+      $('#navBar').css('transform', 'scaleX(1)');
+      $('#header__hamburger').addClass('clicked');
+      setNavState(true);
+    }
+  };
+  return (
+    <>
+      <div className="header">
+        <div className="header__hamburger" id="header__hamburger">
+          <button className="header__hamburger__btn" type="button" onClick={() => toggleNavBar()}> </button>
+        </div>
+      </div>
+      <div className="navBar" id="navBar">
+        <ul className="navBar__list">
+          <li className="navBar__home"> home </li>
+          <li className="navBar__allItems"> all items </li>
+          <li className="navBar__settings"> settings </li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Header;
