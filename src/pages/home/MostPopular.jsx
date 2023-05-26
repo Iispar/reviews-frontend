b9c3240a36507b4ce81d70b8f694cd3f2b1477f6
Iiro Partanen <iispar@utu.fi>
2023-05-26 19:49:45 +0300

    comments open to large. Headet to most popular on home

--- src/pages/home/MostPopular.jsx
+++ src/pages/home/MostPopular.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 // eslint-disable-next-line arrow-body-style
 const MostPopular = () => {
-  return <div> Most popular </div>;
+  return (
+    <div className="mostPopular">
+      <div className="mostPopular__header">
+        <span className="mostPopular__header__text">popular items</span>
+      </div>
+      <div className="mostPopular__list" />
+    </div>
+  );
 };
 
 export default MostPopular;
