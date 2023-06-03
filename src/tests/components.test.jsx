7a557fff4a038778d0a806c5017c13b0cb492edc
Iiro Partanen <iispar@utu.fi>
2023-06-03 18:59:22 +0300

    component tests for footer, header and inputfield

--- src/tests/components.test.jsx
+++ src/tests/components.test.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import $ from 'jquery';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import InputField from '../components/InputField';
 
 const mockedUsedNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
@@ -41,6 +43,22 @@ describe('components render and work', () => {
 
       const afterNavBar = container.querySelector('#navBar');
       expect(afterNavBar).toHaveStyle('transform: scaleX(0)');
+    });
+    test('Input field renders and works', async () => {
+      const view = render(<InputField id="test" title="test" />);
+      const { container } = view;
+      expect(container).toBeTruthy();
+      const inputField = container.querySelector('#test__container__input');
+      expect(inputField).toBeTruthy();
+
+      await user.type(inputField, 'test writing');
+      expect(inputField).toHaveValue('test writing');
+
+      const label = $('#test__container__label').text();
+      expect(label).toBe('test');
+
+      const error = container.querySelector('#test__container__error');
+      expect(error).toBeTruthy();
     });
   });
 });
