7d9c6e4c0703f7d2c2c56cc3dd4d3be238fc8103
Iiro Partanen <iispar@utu.fi>
2023-06-04 13:45:20 +0300

    removed console.logs

--- src/pages/login/loginHooks.js
+++ src/pages/login/loginHooks.js
@@ -1,11 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-len */
+/* eslint-disable arrow-body-style */
 export const UseLogin = (username, password) => {
-  console.log(`Login with username: ${username}, password: ${password}`);
+  // console.log(`Login with username: ${username}, password: ${password}`);
 
   return true;
 };
 
 export const UseCreateAccount = (username, name, email, password) => {
-  console.log(`New account with username: ${username}, name: ${name}, email: ${email}, password: ${password}`);
+  // console.log(`New account with username: ${username}, name: ${name}, email: ${email}, password: ${password}`);
 
   return true;
 };
