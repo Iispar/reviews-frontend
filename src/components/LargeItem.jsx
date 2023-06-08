f0d9fb62a907c841b52ddf20c9b0bf730ad80199
Iiro Partanen <iispar@utu.fi>
2023-06-08 17:58:31 +0300

    large items with basic styling

--- src/components/LargeItem.jsx
+++ src/components/LargeItem.jsx
@@ -1,29 +1,44 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import $ from 'jquery';
 
 // eslint-disable-next-line arrow-body-style
 const LargeItem = (props) => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const { id } = props;
   const { item } = props;
   const { reviews } = props;
   const { rating } = props;
+
+  // wait for the page to load before setting listeners
+  $(document).ready(() => {
+    // navigate to items page when clicked.
+    $(`#largeItem__${id}`).on('click', () => {
+      navigate(`/item/${id}`);
+    });
+  });
+
   return (
     <div className="largeItem" id={`largeItem__${id}`}>
       <div className="largeItem__nameReviews">
-        <span className="largeItem__name__text">
+        <span className="largeItem__nameReviews__name">
           {item}
         </span>
-        <span className="largeItem__name__reviews">
-          {reviews}
-        </span>
+        <div className="largeItem__nameReviews__reviews">
+          <span className="largeItem__nameReviews__reviews__count">
+            {reviews}
+          </span>
+          <span className="largeItem__nameReviews__reviews__label">
+            reviews
+          </span>
+        </div>
       </div>
       <div className="largeItem__rating">
         <span className="largeItem__rating__rate">
-          {' '}
           {rating}
-          {' '}
         </span>
+        <div className="largeItem__rating__star" />
       </div>
     </div>
   );
