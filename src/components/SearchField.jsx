88eda82cfe1352009fe31711c839612af075c255
Iiro Partanen <iispar@utu.fi>
2023-06-08 19:20:46 +0300

    allItems header search and sort layout

---
+++ src/components/SearchField.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// eslint-disable-next-line arrow-body-style
+const SearchField = () => {
+  return (
+    <div className="searchField">
+      <input className="searchField__input" />
+    </div>
+  );
+};
+
+export default SearchField;
--- src/pages/allItems/Items.jsx
+++ src/pages/allItems/Items.jsx
@@ -3,6 +3,7 @@ import Pagination from '../../components/Pagination';
 import ItemList from '../../components/ItemList';
 import LargeItem from '../../components/LargeItem';
 import dummyItems from '../../data/dummyItems.json';
+import SearchField from '../../components/SearchField';
 
 // eslint-disable-next-line arrow-body-style
 const Items = () => {
@@ -10,7 +11,13 @@ const Items = () => {
   return (
     <div className="items">
       <div className="items__header">
-        <span> sort </span>
+        <div className="items__header__search">
+          <SearchField />
+        </div>
+        <div className="items__header__sort">
+          <span className="items__header__sort__text"> sort </span>
+          <div className="items__header__sort__arrow" />
+        </div>
       </div>
       <div className="items__list">
         <ItemList items={items} View={LargeItem} count={6} />
