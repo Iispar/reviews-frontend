b1507b28dff9aacc77e51c781c623917ee9e7742
Iiro Partanen <iispar@utu.fi>
2023-06-09 19:53:42 +0300

    basic sort dropdown menu

--- src/pages/allItems/Items.jsx
+++ src/pages/allItems/Items.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { React, useState } from 'react';
+import $ from 'jquery';
 import Pagination from '../../components/Pagination';
 import ItemList from '../../components/ItemList';
 import LargeItem from '../../components/LargeItem';
@@ -8,15 +9,36 @@ import SearchField from '../../components/SearchField';
 // eslint-disable-next-line arrow-body-style
 const Items = () => {
   const { items } = dummyItems;
+  const [open, setOpen] = useState(false);
+
+  const dropDown = () => {
+    if (open) {
+      $('#items__header__sort').removeClass('expand');
+      $('#items__header__sort__text').css('display', 'flex');
+      $('#items__header__sort__sorts').css('display', 'none');
+      setOpen(false);
+    } else {
+      $('#items__header__sort').addClass('expand');
+      $('#items__header__sort__text').css('display', 'none');
+      $('#items__header__sort__sorts').css('display', 'flex');
+      setOpen(true);
+    }
+  };
+
   return (
     <div className="items">
       <div className="items__header">
         <div className="items__header__search">
           <SearchField />
         </div>
-        <div className="items__header__sort">
-          <span className="items__header__sort__text"> sort </span>
-          <div className="items__header__sort__arrow" />
+        <div className="items__header__sort" id="items__header__sort">
+          <div className="items__header__sort__text" id="items__header__sort__text"> sort </div>
+          <div className="items__header__sort__sorts" id="items__header__sort__sorts">
+            <span className="items__header__sort__sorts__reviews"> reviews </span>
+            <span> rating </span>
+            <span> age? </span>
+          </div>
+          <button className="items__header__sort__arrow" type="button" onClick={() => dropDown()}> </button>
         </div>
       </div>
       <div className="items__list">
