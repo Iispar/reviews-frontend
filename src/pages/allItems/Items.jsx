f3c3ca80f7485c73199168886ab866a0df17444d
Iiro Partanen <iispar@utu.fi>
2023-06-14 15:45:35 +0300

    comments for all items

--- src/pages/allItems/Items.jsx
+++ src/pages/allItems/Items.jsx
@@ -6,11 +6,15 @@ import LargeItem from '../../components/LargeItem';
 import dummyItems from '../../data/dummyItems.json';
 import SearchField from '../../components/SearchField';
 
-// eslint-disable-next-line arrow-body-style
 const Items = () => {
   const { items } = dummyItems;
   const [open, setOpen] = useState(false);
 
+  /**
+   * Sets the hover to a button.
+   * @param {*} id
+   * @param {*} color
+   */
   const hover = (id, color) => {
     $(id).hover(
       () => {
@@ -26,6 +30,8 @@ const Items = () => {
    * Animates and opens the dropdown menu for the sort.
    */
   const dropDown = () => {
+    // we use jquery hover and not css, because we need to disable when clicked.
+    // this doesn't seem that good so will look into a better solution in the future.
     hover('#items__header__sort__reviews__desc', '#f5f5f5');
     hover('#items__header__sort__reviews__asc', '#f5f5f5');
     hover('#items__header__sort__ratings__desc', '#f5f5f5');
@@ -60,6 +66,10 @@ const Items = () => {
     }
   };
 
+  /**
+   * Changes the filter and sets it to displayed.
+   * @param {*} sort
+   */
   const changeFilter = (sort) => {
     const type = sort.split('__')[3];
     let width;
