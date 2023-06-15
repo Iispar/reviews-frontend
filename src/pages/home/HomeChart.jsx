6f99206f2e9e6037e65f04dc50be4a2f95b75927
Iiro Partanen <iispar@utu.fi>
2023-06-15 16:55:24 +0300

    home views stat component all time design

--- src/pages/home/HomeChart.jsx
+++ src/pages/home/HomeChart.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // eslint-disable-next-line arrow-body-style
 const HomeChart = () => {
-  return <div> chart </div>;
+  return <div className="homeChart"> chart </div>;
 };
 
 export default HomeChart;
--- src/pages/home/HomeStats.jsx
+++ src/pages/home/HomeStats.jsx
@@ -2,7 +2,28 @@ import React from 'react';
 
 // eslint-disable-next-line arrow-body-style
 const HomeStats = () => {
-  return <div> stats </div>;
+  return (
+    <div className="homeStats">
+      <div className="homeStats__ratings" />
+      <div className="homeStats__allTime">
+        <div className="homeStats__allTime__stats">
+          <div className="homeStats__allTime__stats__items">
+            <span className="homeStats__allTime__stats__items__value"> 21 </span>
+            <span className="homeStats__allTime__stats__items__label"> items </span>
+          </div>
+          <div className="homeStats__allTime__stats__reviews">
+            <span className="homeStats__allTime__stats__reviews__value"> 320 </span>
+            <span className="homeStats__allTime__stats__reviews__label"> reviews </span>
+          </div>
+          <div className="homeStats__allTime__stats__ratings">
+            <span className="homeStats__allTime__stats__ratings__value"> 3.2 </span>
+            <span className="homeStats__allTime__stats__ratings__label"> avg rating </span>
+          </div>
+        </div>
+        <div className="homeStats__allTime__title"> all time</div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeStats;
